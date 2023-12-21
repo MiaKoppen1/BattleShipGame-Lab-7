ea9da55ffb5ece63d2e5d36e5ca43662b68d8009
mia <143561609+MiaKoppen1@users.noreply.github.com>
2023-12-20 23:22:38 -0800

    had to manually copy and paste as well

--- Lab No#7/Battleship.h
+++ Lab No#7/Battleship.h
@@ -9,23 +9,24 @@ class BattleShips
     static const int rows = 10;
     static const int columns = 10;
     int matrix[rows][columns];
-
     BattleShips();
     void ClearGrid();
     void ShowGrid();
     int numberOfShips();
-    void setShips();
+    void setShips(bool isPlayer);
     bool Attack(int _x, int _y);
+    bool isValidPlacement(int xPos, int yPos, char direction);
+    void placeShip(int xPos, int yPos, char direction);
     };
 
 class Boards 
     {
-
     public:
-    void SetGridColor(int color);
+    int color;
     void MakeBoards();
     void updateBoards(bool i, int x, int y);
     void PrintBoards();
+    void setColor(int newColor); // New method to set the color
     static const int rows = 10;
     static const int columns = 10;
     char grid[rows][columns];
